ffb1dd53c70f70fbc4c86b564c2e1785ba76ea50
ivwhiting <ivwhiting@239dd4bf-7d30-0410-957d-cf47c937f3a9>
2007-09-07 08:42:28 +0000

    Some updates. These updates will temporarily remove features for those building from source.

--- trunk/viewstate.cpp
+++ trunk/viewstate.cpp
@@ -27,21 +27,36 @@
 #include "type_convert.h"
 #include "base64.h"
 
-// Colour Console Output
+// Colour Console Output...
+#if !defined(__WIN32__)
 const char *RESET = "[0m";			// DEFAULT
 const char *COL_RED = "[31m";		// RED
 const char *COL_BLUE = "[34m";		// BLUE
 const char *COL_GREEN = "[32m";	// GREEN
+#else
+const char *RESET = "";
+const char *COL_RED = "";
+const char *COL_BLUE = "";
+const char *COL_GREEN = "";
+#endif
 
 // Program banner and help
-const string bannerlogo = "            _                   _        _\n     __   _(_) _____      _____| |_ __ _| |_ ___\n     \\ \\ / / |/ _ \\ \\ /\\ / / __| __/ _` | __/ _ \\\n      \\ V /| |  __/\\ V  V /\\__ \\ || (_| | ||  __/\n       \\_/ |_|\\___| \\_/\\_/ |___/\\__\\__,_|\\__\\___|";
-const string version = "                     Version 2.0.0\n             by Ian Ventura-Whiting (Fizz)";
+const string progbanner = "             _                   _        _\n"
+                          "      __   _(_) _____      _____| |_ __ _| |_ ___\n"
+                          "      \\ \\ / / |/ _ \\ \\ /\\ / / __| __/ _` | __/ _ \\\n"
+                          "       \\ V /| |  __/\\ V  V /\\__ \\ || (_| | ||  __/\n"
+                          "        \\_/ |_|\\___| \\_/\\_/ |___/\\__\\__,_|\\__\\___|\n\n"
+                          "                      Version 2.0.0\n"
+                          "              by Ian Ventura-Whiting (Fizz)\n"
+                          "                viewstate.sourceforge.net";
+const string progver = "Viewstate 2.0.0 by Ian Ventura-Whiting (Fizz)";
 
 #define mode_none 0
-#define mode_error 1
-#define mode_encode 2
-#define mode_decode 3
-#define mode_check 4
+#define mode_help 1
+#define mode_version 2
+#define mode_error 3
+#define mode_encode 4
+#define mode_decode 5
 
 #define noise_quiet 0
 #define noise_normal 1
@@ -50,15 +65,17 @@ const string version = "                     Version 2.0.0\n             by Ian 
 
 struct programOptions
 {
+	// Files...
+	char *inputFilename;				// Input file
+	char *outputFilename;				// Output file
+	char *hashFilename;					// File containing a hash
+
 	// Options...
-	int inputFile;						// Points to commandline arguement containing input file
-	int outputFile;						// Points to commandline arguement containing output file
 	int viewstateMode;					// Mode for the viewstate program
 	int noise;							// Output noise
 	bool htmlInput;						// Treat input as HTML
 	bool showHash;						// Show the viewstate hash (if one exists)
 	bool parseInput;					// Parse the viewstate data
-	int hashFile;						// Points to commandline arguement containing a hash file
 	bool urlSafe;						// URL safe output option for encoded viewstate
 };
 
@@ -67,30 +84,24 @@ int main(int argc, char *argv[])
 {
 	// Variables...
 	struct programOptions options;
-	int fileLength;
-	char *fileBuffer;
-	string tempString;
-	ifstream hashFile;
 	ifstream inFile;
 	ofstream outFile;
-	string inputString;
-	string outputString;
-	bool showHelp = false;				// Show on-line help
+	ifstream hashFile;
 
-	// Defaults...
-	options.inputFile = 0;				// Points to commandline arguement containing input file
-	options.outputFile = 0;				// Points to commandline arguement containing output file
+	// Init...
+	memset(&options, 0, sizeof(options));
 	options.viewstateMode = mode_none;	// Mode for the viewstate program
 	options.noise = noise_normal;		// Quiet option
 	options.htmlInput = false;			// Treat input as HTML
-	options.showHash = false;			// Show the viewstate hash (if one exists)
+	options.showHash = true;			// Show the viewstate hash (if one exists)
 	options.parseInput = false;			// Parse the viewstate data
-	options.hashFile = 0;				// Points to commandline arguement containing a hash file
 	options.urlSafe = false;			// URL safe output option for encoded viewstate
 
 	// Process command line options...
 	for (int loop = 1; loop < argc; loop++)
 	{
+		// MODES...
+
 		// If decode mode
 		if (strcmp(argv[loop], "--decode") == 0)
 		{
@@ -109,27 +120,33 @@ int main(int argc, char *argv[])
 				options.viewstateMode = mode_error;
 		}
 
-		// check mode
-		else if (strcmp(argv[loop], "--check") == 0)
+		// help mode
+		else if (strcmp(argv[loop], "--help") == 0)
 		{
 			if (options.viewstateMode == mode_none)
-				options.viewstateMode = mode_check;
+				options.viewstateMode = mode_help;
 			else
 				options.viewstateMode = mode_error;
 		}
 
+		// version mode
+		else if (strcmp(argv[loop], "--version") == 0)
+		{
+			if (options.viewstateMode == mode_none)
+				options.viewstateMode = mode_version;
+			else
+				options.viewstateMode = mode_error;
+		}
+
+		// GENERAL OPTIONS...
+
 		// Input file
-		else if (strncmp(argv[loop], "--input=", 8) == 0)
-			options.inputFile = loop;
+		else if ((strncmp(argv[loop], "--input=", 8) == 0) && (strlen(argv[loop]) > 8))
+			options.inputFilename = argv[loop] + 8;
 
 		// Output file
-		else if (strncmp(argv[loop], "--output=", 9) == 0)
-		{
-			if (options.viewstateMode == mode_check)
-				showHelp = true;
-			else
-				options.outputFile = loop;
-		}
+		else if ((strncmp(argv[loop], "--output=", 9) == 0) && (strlen(argv[loop]) > 9))
+			options.outputFilename = argv[loop] + 9;
 
 		// Quiet option
 		else if (strcmp(argv[loop], "--quiet") == 0)
@@ -139,187 +156,208 @@ int main(int argc, char *argv[])
 		else if (strcmp(argv[loop], "--verbose") == 0)
 			options.noise = noise_verbose;
 
+		// DECODE OPTIONS...
+
 		// HTML input option
 		else if (strcmp(argv[loop], "--html") == 0)
 		{
 			if (options.viewstateMode == mode_encode)
-				showHelp = true;
+				options.viewstateMode = mode_error;
 			else
 				options.htmlInput = true;
 		}
 
-/*		// Show hash option
-		else if (strcmp(argv[loop], "--showhash") == 0)
+		// Show hash option
+		else if (strcmp(argv[loop], "--no-hash") == 0)
 		{
 			if (options.viewstateMode == mode_encode)
-				showHelp = true;
+				options.viewstateMode = mode_error;
 			else
-				options.showHash = true;
+				options.showHash = false;
 		}
 
 		// Parse option
 		else if (strcmp(argv[loop], "--parse") == 0)
 		{
 			if (options.viewstateMode == mode_encode)
-				showHelp = true;
+				options.viewstateMode = mode_error;
 			else
 				options.parseInput = true;
-		}*/
+		}
+
+		// ENCODE OPTIONS...
 
 		// Hash file
-		else if (strncmp(argv[loop], "--hash=", 7) == 0)
+		else if ((strncmp(argv[loop], "--hash=", 7) == 0) && (strlen(argv[loop]) > 7))
 		{
-			if (options.viewstateMode != mode_encode)
-				showHelp = true;
+			if (options.viewstateMode == mode_decode)
+				options.viewstateMode = mode_error;
 			else
-				options.hashFile = loop;
+				options.hashFilename = argv[loop] + 7;
 		}
 
 		// URL Safe option
-		else if (strcmp(argv[loop], "--urlSafe") == 0)
+		else if (strcmp(argv[loop], "--urlsafe") == 0)
 		{
-			if (options.viewstateMode != mode_encode)
-				showHelp = true;
+			if (options.viewstateMode == mode_decode)
+				options.viewstateMode = mode_error;
 			else
 				options.urlSafe = true;
 		}
 
 		// Catch all other command line parameters
 		else
-			showHelp = true;
+			options.viewstateMode = mode_error;
 	}
 
-	// Show help...
-	if ((showHelp == true) || (options.viewstateMode == mode_none) || (options.viewstateMode == mode_error))
+	switch (options.viewstateMode)
 	{
-		// Program banner and version...
-		cout << COL_BLUE << bannerlogo << RESET << endl << endl;
-		cout << version << endl << endl;
+		// Program version...
+		case mode_version:
+			cout << progver << endl;
+			break;
 
-		// Program description...
-		cout << "   Web pages provided by ASP.net will typically contain" << endl;
-		cout << "   viewstate data. Viewstate is used by ASP.net to" << endl;
-		cout << "   maintain state between web pages. This program is" << endl;
-		cout << "   designed to decode, encode or check the ASP.net" << endl;
-		cout << "   viewstate data provided by ASP.net." <<endl << endl;
+		// An error occured...
+		case mode_none:
+		case mode_error:
+			cout << COL_RED << "ERROR: Invalid commandline arguements!" << endl << endl;
 
-		// Program command
-		cout << COL_BLUE << "Command:" << RESET << endl << endl;
-		cout << COL_GREEN << "   " << argv[0] << " <mode> [options]" << RESET << endl << endl;
+		// Show help...
+		case mode_help:
+			cout << COL_BLUE << progbanner << RESET << endl << endl;
+			// Program description...
+			cout << "Viewstate is a small tool that can decode, encode and verify" << endl;
+			cout << "ASP .Net viewstate data.  Viewstate data is used to maintain" << endl;
+			cout << "state between  web pages and  includes some  simple security" << endl;
+			cout << "options." << endl << endl;
+			cout << "At present Viewstate  cannot decrypt  encrypted data  and it" << endl;
+			cout << "doesn't generate  a hash  when encoding.  If you know of how" << endl;
+			cout << "these can be done, get in touch and I'll add it. :-)" << endl << endl;
 
-		// Program modes
-		cout << COL_BLUE << "Modes:" << RESET << endl << endl;
+			// Program command
+			cout << COL_BLUE << "Command:" << RESET << endl << endl;
+			cout << COL_GREEN << "   " << argv[0] << " <mode> [options]" << RESET << endl << endl;
 
-		cout << COL_GREEN << "   --decode" << RESET << endl;
-		cout << "   Decode the encoded viewstate data, if possible." << endl << endl;
+			// Program modes
+			cout << COL_BLUE << "Modes:" << RESET << endl << endl;
 
-		cout << COL_GREEN << "   --encode" << RESET << endl;
-		cout << "   Encode the viewstate data." << endl << endl;
+			cout << COL_GREEN << "   --decode" << RESET << endl;
+			cout << "   If possible, decode the supplied viewstate data." << endl << endl;
 
-		cout << COL_GREEN << "   --check" << RESET << endl;
-		cout << "   Decode the encoded viewstate data, but not output." << endl << endl;
+			cout << COL_GREEN << "   --encode" << RESET << endl;
+			cout << "   Encode the supplied viewstate data." << endl << endl;
 
-		// Program options
-		cout << COL_BLUE << "General Options:" << RESET << endl << endl;
+			cout << COL_GREEN << "   --version" << RESET << endl;
+			cout << "   Display the program version." << endl << endl;
 
-		cout << COL_GREEN << "   --help" << RESET << endl;
-		cout << "   Display this command line help. Overrides all other" << endl;
-		cout << "   options." << endl << endl;
+			cout << COL_GREEN << "   --help" << RESET << endl;
+			cout << "   Show this on-line help text you are reading now." << endl << endl;
 
-		cout << COL_GREEN << "   --input=<file>" << RESET << endl;
-		cout << "   The input file for processing. If not specified, the" << endl;
-		cout << "   input is retrieved from stdin." << endl << endl;
+			// General options
+			cout << COL_BLUE << "General Options:" << RESET << endl;
+			cout << "   The general  options can  be used  with either  encode or" <<endl;
+			cout << "   decode modes." << endl << endl;
 
-		cout << COL_GREEN << "   --output=<file>" << RESET << endl;
-		cout << "   The output file for the result of an encoding or" <<endl;
-		cout << "   decoding operation. If not specified, the output" << endl;
-		cout << "   is sent to stdout." << endl << endl;
+			cout << COL_GREEN << "   --input=<file>" << RESET << endl;
+			cout << "   An input file for processing. If not specified, the input" << endl;
+			cout << "   is retrieved from stdin." << endl << endl;
 
-		cout << COL_GREEN << "   --quiet" << RESET << endl;
-		cout << "   Do not show the processing information." << endl << endl;
+			cout << COL_GREEN << "   --output=<file>" << RESET << endl;
+			cout << "   The  output file for the  result.  If  not specified  the" << endl;
+			cout << "   output is sent to stdout." << endl << endl;
 
-		cout << COL_GREEN << "   --verbose" << RESET << endl;
-		cout << "   Show additional debug output." << endl << endl;
+			cout << COL_GREEN << "   --quiet" << RESET << endl;
+			cout << "   Do not show the processing information." << endl << endl;
 
-		cout << COL_BLUE << "Decode/Check Mode Options:" << RESET << endl << endl;
+			cout << COL_GREEN << "   --verbose" << RESET << endl;
+			cout << "   Show additional debug output." << endl << endl;
 
-		cout << COL_GREEN << "   --html" << RESET << endl;
-		cout << "   The input file for processing should be treated as" << endl;
-		cout << "   raw HTML. The default is to treat the input as raw" << endl;
-		cout << "   viewstate data." << endl << endl;
+			// Decode options
+			cout << COL_BLUE << "Decode Mode Options:" << RESET << endl;
+			cout << "   These  options  are  for   viewstate  decoding.   If  the" << endl;
+			cout << "   viewstate data is encrypted, the encrypted data is shown." << endl << endl;
 
-		/*cout << COL_GREEN << "   --showhash" << RESET << endl;
-		cout << "   Show the viewstate hash, if one exists. Viewstate" << endl;
-		cout << "   has a hash appended by default, if a hash doesn't" << endl;
-		cout << "   exist then it is possible to modify the data and" << endl;
-		cout << "   submit a modified viewstate back to the server." << endl << endl;
+			cout << COL_GREEN << "   --html" << RESET << endl;
+			cout << "   Treat the  input file as raw HTML.  If this option is not" << endl;
+			cout << "   specified  the input  will be  treated as  raw  viewstate" << endl;
+			cout << "   data." << endl << endl;
 
-		cout << COL_GREEN << "   --parse" << RESET << endl;
-		cout << "   Parse the viewstate data to make it more human" << endl;
-		cout << "   readable and output to stdout." << endl << endl;*/
+			cout << COL_GREEN << "   --no-hash" << RESET << endl;
+			cout << "   Do not show the viewstate hash, if one exists." << endl << endl;
 
-		cout << COL_BLUE << "Encode Mode Options:" << RESET << endl << endl;
+			cout << COL_GREEN << "   --parse" << RESET << endl;
+			cout << "   Parse the viewstate  data to make it  more human readable" << endl;
+			cout << "   and output to stdout.  At present,  only the old style of" << endl;
+			cout << "   viewstate is parsed." << endl << endl;
 
-		cout << COL_GREEN << "   --hash=<file>" << RESET << endl;
-		cout << "   Append a hash to the encoded viewstate data." << endl << endl;
+			// Encode options
+			cout << COL_BLUE << "Encode Mode Options:" << RESET << endl;
+			cout << "   These are for viewstate encoding." << endl << endl;
 
-		cout << COL_GREEN << "   --urlsafe" << RESET << endl;
-		cout << "   Make the output safe to use in a URL." << endl << endl;
-	}
+			cout << COL_GREEN << "   --hash=<file>" << RESET << endl;
+			cout << "   Append a  hash to  the encoded  viewstate data.  The hash" << endl;
+			cout << "   must be calculated and valid,  else it is rejected by the" << endl;
+			cout << "   server." << endl << endl;
 
-	// Lets do it!
-	else
-	{
-		// Display program banner and version...
-		if (options.noise != noise_quiet)
-			cout << COL_BLUE << bannerlogo << RESET << endl << endl << version << endl << endl;
+			cout << COL_GREEN << "   --urlsafe" << RESET << endl;
+			cout << "   Make the output safe to use in a URL." << endl << endl;
+			break;
 
-		// Display command line options
-		if (options.noise == noise_verbose)
-		{
-			cout << COL_GREEN << "Command: " << RESET;
-			for (int loop = 0; loop < argc; loop++)
+		case mode_decode:
+		case mode_encode:
+			// Display program banner and version...
+			if (options.noise != noise_quiet)
+				cout << COL_BLUE << progbanner << RESET << endl << endl;
+
+			// Display command line options...
+			if (options.noise == noise_verbose)
 			{
-				cout << argv[loop] << " ";
+				cout << COL_GREEN << "Command: " << RESET;
+				for (int loop = 0; loop < argc; loop++)
+					cout << argv[loop] << " ";
+				cout << endl;
 			}
-			cout << endl;
-		}
-		if (options.noise != noise_quiet)
-		{
-			cout << COL_GREEN << "Mode   : " << RESET;
-			switch (options.viewstateMode)
+
+			if (options.noise != noise_quiet)
 			{
-				case mode_decode:
-					cout << "Decode" << endl;
-					break;
-				case mode_encode:
-					cout << "Encode" << endl;
-					break;
-				case mode_check:
-					cout << "Check" << endl;
-					break;
-			}
-			cout << COL_GREEN << "Input  : " << RESET;
-			if (options.inputFile != 0)
-				cout << argv[options.inputFile] + 8 << endl;
-			else
-				cout << "stdin" << endl;
-			if (options.viewstateMode != mode_check)
-			{
-				cout << COL_GREEN << "Output : " << RESET;
-				if (options.outputFile != 0)
-					cout << argv[options.outputFile] + 9 << endl;
+				// Show mode...
+				if (options.viewstateMode == mode_decode)
+					cout << COL_GREEN << "Mode   : " << RESET << "Decode" << endl;
 				else
-					cout << "stdout" << endl;
+					cout << COL_GREEN << "Mode   : " << RESET << "Encode" << endl;
+
+				// Show Input...
+				if (options.inputFilename != 0)
+					cout << COL_GREEN << "Input  : " << RESET << options.inputFilename << endl;
+				else
+					cout << COL_GREEN << "Input  : " << RESET << "stdin" << endl;
+
+				// Show Output...
+				if (options.outputFilename != 0)
+					cout << COL_GREEN << "Output : " << RESET << options.outputFilename << endl;
+				else
+					cout << COL_GREEN << "Output : " << RESET << "stdout" << endl;
+
+				// Hash file...
+				if (options.hashFilename != 0)
+					cout << COL_GREEN << "Hash   : " << RESET << options.hashFilename << endl;
 			}
-			cout << endl;
-		}
+
+			// Opening input...
+			if (options.noise == noise_verbose)
+				cout << COL_GREEN << "Opening input:" << RESET;
+			if (options.inputFilename != 0)
+			{
+			}
+			break;
+	}
+}
+
+/*
 
 		// Open input file
-		if (options.inputFile != 0)
-		{
-			if (options.noise == noise_verbose)
-				cout << COL_GREEN << "Opening file:" << RESET;
+		
+			
 			inFile.open(argv[options.inputFile] + 8);
 
 			// File open ok
@@ -449,8 +487,8 @@ int main(int argc, char *argv[])
 							if (options.noise == noise_verbose)
 								cout << " OK" << endl;
 
-							// Check mode, output viewstate type...
-							if ((options.viewstateMode == mode_check) || (options.noise != noise_quiet))
+							// Output viewstate type...
+							if (options.noise != noise_quiet)
 							{
 								cout << COL_GREEN << "Viewstate Type: " << RESET;
 								if (outputString[0] == -1)
@@ -565,6 +603,5 @@ int main(int argc, char *argv[])
 			cout << COL_RED << "ERROR: Could not open input file!" << RESET << endl;
 		}
 	}
+*/
 
-	return EXIT_SUCCESS;
-}
